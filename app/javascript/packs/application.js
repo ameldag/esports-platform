8834bb56fe335a6892da7c099e9b601a7d1fac4d
Slim Ben Nasrallah <slimbennasrallah@gmail.com>
2020-05-12 15:01:33 +0200

    added the tabs for team

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,24 @@ import '../css/application.css'
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// import "controllers"
+
+// added that part
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+
+
+// Import and register all TailwindCSS Components
+import { Dropdown, Modal, Tabs, Popover, Toggle } from "tailwindcss-stimulus-components"
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
