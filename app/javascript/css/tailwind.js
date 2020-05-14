7dd919c9fe5aa252f4668f5f8e764755a2a23fe2
Slim Ben Nasrallah <slimbennasrallah@gmail.com>
2020-05-14 14:59:17 +0200

    fixed the teams interface

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,6 +1,9 @@
 module.exports = {
   purge: [],
   theme: {
+    boxShadow: {
+      white: '0 4px 6px -1px rgba(254, 254, 254, .1), 0 2px 4px -1px rgba(254, 254, 254, .06)'
+    },
     extend: {},
   },
   variants: {},
