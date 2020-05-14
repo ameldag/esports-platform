5c6213a4d01f6102a00bbb0a8bfe0c8e2441b9e7
Slim Ben Nasrallah <slimbennasrallah@gmail.com>
2020-05-15 00:44:04 +0200

    created the edit user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,43 @@ class UsersController < ApplicationController
     end
   
     def show
-      @team = Team.find(params[:id])
-      @requests = Request.where('team_id = ? and user_id != ? and status = ?', @team.id, current_user.id, "pending").all
-      @members = @team.users
-      
-      @current_user_request = Request.where('team_id = ? and user_id = ? and status = ?', @team.id, current_user.id, "pending").count
+        @team = Team.find(params[:id])
+        @requests = Request.where('team_id = ? and user_id != ? and status = ?', @team.id, current_user.id, "pending").all
+        @members = @team.users
+        
+        @current_user_request = Request.where('team_id = ? and user_id = ? and status = ?', @team.id, current_user.id, "pending").count
     end
+
+      
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    # PATCH/PUT /users/1
+    # PATCH/PUT /users/1.json
+    def update
+
+        @user = User.find(params[:id])
+
+        respond_to do |format|
+            
+            if @user.update(user_params)
+                format.html { redirect_to root_path, notice: 'Your profile was successfully updated.' }
+                format.json { render :show, status: :ok, location: @user }
+            else
+                format.html { render :edit }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
+            end
+
+        end
+    end
+
   
     private
-  
 
-  end
+    def user_params
+        params.require(:user).permit(:username, :first_name, :last_name, :avatar, :cover)
+    end
+
+end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, uniqueness: true
+
   has_many :requests, dependent: :destroy
   
   has_many :users_teams, dependent: :destroy
   has_many :teams, through: :users_teams, dependent: :destroy
+  
+  
+
+  has_one_attached :avatar, dependent: :destroy
+  has_one_attached :cover, dependent: :destroy
 end
