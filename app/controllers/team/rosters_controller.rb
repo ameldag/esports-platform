e2cf6a732b1d92ce4736e323080eeedf58ad4ac5
Slim Ben Nasrallah <slimbennasrallah@gmail.com>
2020-06-10 13:12:37 +0200

    added algo in comments

--- app/controllers/team/rosters_controller.rb
+++ app/controllers/team/rosters_controller.rb
@@ -12,9 +12,33 @@ class Team::RostersController < ApplicationController
 
   def join
     @roster = Roster.find(params[:id])
+    # verifier si le roster en question existe ou non
+    # autrement rediriger vers la page de la team et dire que le roster recherché n'existe pas
     
-    # doit verifier request de type roster 
-    @current_user_request = Request.where('user_id = ? and status = ? and target = ? ',current_user.id, "pending", "roster").count 
+    # verifier si l'utilisateur appartient à une team
+      # appartient à une team
+      # si c'est la meme team
+        # doit verifier request de type roster 
+        # Actuellement il faut verifier si l'utilisateur a une request pour rejoindre CE roster
+        @current_user_request = Request.where('user_id = ? and status = ? and target = ? and roster = ?',current_user.id, "pending", "roster", @roster).count 
+        
+        # si l'utilisateur a déjà une request en cours
+          # le rediriger vers la page du roster et lui dire que sa demande est toujours en cours
+        # autrement
+          # verifier si l'utilisateur est déjà membre de roster
+            # rediriger vers la page du roster et lui dire qu'il est deja membre
+          # autrement
+            # creer une request
+            # envoyer un email pour dire que la requete est bien faite
+            # envoyer un email au manager de la team
+            # envoyer une notification au manager de la team
+            # rediriger vers la page du roster
+          #end
+        # end
+      # si autre team
+        # rediriger et dire bye bye habibi
+      # end
+    # end
     if (!current_user.team.rosters.nil?)
       respond_to do |format|
         format.html { redirect_to show_roster_path(current_user.team, @roster), alert: 'You already belong to this roster' }
@@ -53,10 +77,46 @@ class Team::RostersController < ApplicationController
   end
 
   def join_request_answer
+    # verifier si current_user is team owner
+      # si oui
+        # verifier si la request existe
+          # verifier que le roster visé appartient à la team
+          # si la réponse est positive
+            # verifier si l'utisateur est membre de la team
+              # si oui
+                # ajouter au roster
+                # effacer la request
+                # redirect to requests page
+              # si false
+                # si il est membre d'une autre team
+                  # supprimer la request
+                  # rediriger vers la page de requests et dire que l'utilisateur "l'utilisateur est déjà membre d'une autre team et ne peux rejoindre le roster de la votre"
+                  # email au submitter pour dire que la demande ne peut aboutir car il appartient déjà à une autre team
+          # si la réponse est négative
+            # supprimer la request
+            # envoyer un email à l'utilisateur
+            # rediriger vers la page de requests
+        # si la request n'existe pas
+          # rediriger et dire que la request n'existe pas
+      # si non
+        # rediriger et dire que vous n'avez suffisament de droits pour effectuer cette action
+    # end
+
   end
 
   def quit
     @roster = Roster.find(params[:id])
+    # verifier si le roster en question existe ou non
+    # autrement rediriger vers la page de la team et dire que le roster recherché n'existe pas
+
+    # verifier si current_user appartient au roster ou non
+      # current user appartient, on le enleve le record correspondant au user and au roster de la table de jointure
+      # on redirige vers la page du roster, et dire que l'utilisateur a quitté le roster
+    # autrement
+      # rediriger vers la page du roster
+      # dire que l'utilisateur n'appartient pas à ce roster pour pouvoir le quitter
+    # end
+
     if (@roster.team.owner_id == current_user.id)
       respond_to do |format|
         format.html { redirect_to show_roster_path(@roster), alert: 'You cannot quit the as a founder until you designate another team owner' }
@@ -85,9 +145,6 @@ class Team::RostersController < ApplicationController
       end
       
     end
-  end
-
-  def add
   end
 
   def edit
