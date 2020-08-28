d81230c2fe91df88be0a6ff2cccf555cb9a4b3c8
NidhalDjerbi <nidhaldjerbi@gmail.com>
2020-08-28 17:18:19 +0200

    game seed

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,6 +5,7 @@ class GameController < ApplicationController
 
   def show
     # Listing tournaments in the home page
+    @count_tournament = Tournament.where("game_id = ?", @game.id).count()
     @ongoing_tournaments = Tournament.where('active = ? and game_id = ?', true, @game.id).where('end_date > ?', Date.today).last(4)
   end
 
