c22dce6f9f6b8b42fd2173e416cf481f4b569c89
Amel Dagdoug <amel.d@seemba.cloud>
2021-02-10 00:08:59 +0100

    test

--- lib/match_manager/csgo_match_manager.rb
+++ lib/match_manager/csgo_match_manager.rb
@@ -4,14 +4,13 @@ require "./lib/match_manager/match_manager"
 class CsgoMatchManager < SeembaEsports::MatchManager
   game_name = "CSGO"
   def self.start_match(match)
-    client = Rcon::Client.new(host: match.server.hostname, port: match.server.port.to_i, password: match.server.rcon_password)
+    client = Rcon::Client.new(host: "127.0.0.1", port: match.server.port.to_i, password: match.server.rcon_password)
     client.authenticate!
     client.execute("cvarlist")
     client.execute("log on")
-    puts "test csgo ************"
-    # client.execute("logaddress_add_http " "\"https://seemba-esports.herokuapp.com/csgolog/" + match.id.to_s + "\"")
-    # client.execute("mp_teamname_1 " + match.left_team.name)
-    # client.execute("mp_teamname_2 " + match.right_team.name)
-    # client.execute("bot_add_t")
+    client.execute("logaddress_add_http " "\"https://seemba-esports.herokuapp.com/csgolog/" + match.id.to_s + "\"")
+    client.execute("mp_teamname_1 " + match.left_team.name)
+    client.execute("mp_teamname_2 " + match.right_team.name)
+    client.execute("bot_add_t")
   end
 end
