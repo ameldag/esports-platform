75c097d11b2c0f5a8b5ebba035031456a05dfcf2
NidhalDjerbi <nidhaldjerbi@gmail.com>
2021-02-18 10:29:15 +0100

    refactor match manger

--- lib/match_manager/match_manager.rb
+++ lib/match_manager/match_manager.rb
@@ -1,9 +1,15 @@
 module SeembaEsports
   class MatchManager < ActiveRecord::Base
-    attr_accessor :game_name
+    attr_accessor :game_name, :str, :match
     self.abstract_class = true
 
     def start_match(game_name)
+      raise NotImplementedError, "Subclasses must define `start_match`."
+    end
+    def csgologs(str,match)
+      raise NotImplementedError, "Subclasses must define `start_match`."
+    end
+    def parse_score(match)
       raise NotImplementedError, "Subclasses must define `start_match`."
     end
   end
